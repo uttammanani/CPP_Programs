73268c06a9d64ba3298ad7c0bc9a96dba6f4e696
Uttam Manani <uttammanani@gmail.com>
2020-09-21 10:25:22 +0530

    Add files via upload
    In this program, following measures were taken to calculate tax and net income:
    ___________________________________________
    | Income                               | Tax Rate     |
    |------------------------------|-------------|
    |<=500000                           | No Tax       |
    |>500000&&<=1000000     | 10% Tax     |
    |Above 1000000                   | 20% Tax     |
    ---------------------------------------------

---
+++ TaxRate_NetIncome.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //int i;
+    double income,tax=0, netincome=0;
+    char ch;
+
+    do
+    {
+    cout<<"Enter Your Annual Income: ";
+    cin>>income;
+
+    if(income<0)
+    {
+        cout<<"Please Enter Valid Income!";
+    }
+
+    if(income<=500000 && income>=0)
+    {
+        tax=0;
+    }
+
+    if(income>500000 && income<=1000000)
+    {
+        tax=10;
+    }
+
+    if(income>1000000)
+    {
+        tax=20;
+    }
+
+    netincome=income-(income*(tax/100));
+
+    cout<<"Income After Tax Is "<<netincome<<endl;
+
+    cout<<"Do You Want To Check Again?(Y/N): ";
+    cin>>ch;
+
+    }while (ch=='y' || ch=='Y');
+
+    return 0;
+}
